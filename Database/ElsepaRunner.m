1e9ce36cc67180925b802c8cc3776b6b4a3e9316
Olga Ridzel <olgaridzel@Olgas-MacBook-Air.local>
2023-12-02 17:07:41 -0500

    mac changes

--- Database/ElsepaRunner.m
+++ Database/ElsepaRunner.m
@@ -3,7 +3,8 @@ classdef ElsepaRunner
     methods (Static)
         function [Res] = RunElsepa(Composition, E0)
             if isempty(E0); return; end
-            dir_Elsepa = 'C:/Users/onr5/OneDrive - NIST/dev/elsepa-2020';
+            % dir_Elsepa = 'C:/Users/onr5/OneDrive - NIST/dev/elsepa-2020';
+            dir_Elsepa = '/Users/olgaridzel/olgaridzel/dev/elsepa-2020';
             f_name = fullfile(dir_Elsepa,'lub.in');
             sumweights = sum(Composition.index);
             sigma_el = zeros(length(Composition.Z),numel(E0));
@@ -33,7 +34,7 @@ classdef ElsepaRunner
                 fclose(fid);
 
                 cd(dir_Elsepa);
-                system('elsepa < lub.in');
+                system('./elsepa < lub.in');
                     
                 DELIMITER = ' ';
             
--- Database/MaterialData_script.m
+++ Database/MaterialData_script.m
@@ -2,8 +2,9 @@ clear
 
 MaterialData = {};
 
-MaterialData.Si = Make_Si;
-MaterialData.Si_DFT = Make_Si_DFT;
 MaterialData.PMMA = Make_PMMA;
+MaterialData.Si = Make_Si;
+MaterialData.SiO2 = Make_SiO2;
+MaterialData.Si_DFT = Make_Si_DFT;
 
 clear Si Si_DFT PMMA
