f889c2886bf76031d93519506bc4481f67230d7e
Olga Ridzel <olgaridzel@Olgas-MacBook-Air.local>
2023-12-03 13:40:22 -0500

    fixed simulation after parfor

--- Lib/simulateSEE.m
+++ Lib/simulateSEE.m
@@ -4,7 +4,7 @@ function e = simulateSEE(N_traj,E0,MatName)
     e = cell(N_traj,1);
     % parpool(2)
 
-    parfor i = 1:N_traj
+    for i = 1:N_traj
         e{i}(1) = Electron(E0,Mat);
         e_count = 0;
         while e_count < length(e{i})
@@ -23,7 +23,7 @@ function e = simulateSEE(N_traj,E0,MatName)
                             uvw = [ sin(acos(2*rand-1))*cos(2*rand*pi),...
                                     sin(acos(2*rand-1))*sin(2*rand*pi),...
                                     cos(acos(2*rand-1)) ];
-                            e{i}(e_count + 1) = Electron(e_se,Mat,e{i}(e_count).xyz,uvw,e{i}(e_count).nSecondaries+1,true);
+                            e{i}(end + 1) = Electron(e_se,Mat,e{i}(e_count).xyz,uvw,e{i}(e_count).nSecondaries+1,true);
                             e{i}(e_count).nSecondaries = e{i}(e_count).nSecondaries + 1;
                         end
                     end
--- test.m
+++ test.m
@@ -1,23 +1,26 @@
 clear;
 
-N = 1000;
-E0 = [50 100 200 500 700 1000];
+N = 10000;
+E0 = [25 50 75 100 200 500 700 1000];
 e = cell(length(E0),1);
 matname = 'Si';
 
-tic
+tStart = tic;
 for i = 1:length(E0)
     disp(E0(i))
+    tic
     e{i} = simulateSEE(N,E0(i),matname);
+    toc
 end
-toc
+tEnd = toc(tStart);
+disp(['Total elapsed time over energies is ' num2str(tEnd) ' seconds.'])
 
 %% Ready results
 % load Si_DFT_e_array.mat;
 
 %% Histograms
-% spec_se = zeros(1);
-% spec_pe = zeros(1);
+spec_se = cell(size(E0));
+spec_pe = cell(size(E0));
 sey = zeros(size(E0));
 bse = zeros(size(E0));
 for i = 1:length(E0)
@@ -26,14 +29,17 @@ for i = 1:length(E0)
     for j = 1:N
         for k = 1:length(e{i}{j})
             if ~e{i}{j}(k).Inside && ~e{i}{j}(k).Dead
-                if e{i}{j}(k).isSecondary                    
+                if e{i}{j}(k).isSecondary
+                    spec_se{i}(end+1) = e{i}{j}(k).Energy;
                     sey(i) = sey(i) + 1;
                 else
+                    spec_pe{i}(end+1) = e{i}{j}(k).Energy;  
                     bse(i) = bse(i) + 1;
                 end
             end
         end
     end
+    disp([num2str(E0(i)) '  ' num2str((sey(i)+bse(i))/N)])
 end
 
 %% Yields
@@ -55,8 +61,8 @@ legend
 %{
 figure
 hold on
-histogram(spec_pe,500)
-histogram(spec_se,500)
+histogram(spec_se{5},"NumBins",100)
+histogram(spec_pe{5},"NumBins",100)
 xlabel('Electron energy (eV)')
 %}
 
